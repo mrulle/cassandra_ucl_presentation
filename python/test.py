8140354f4c495939ec296bb04591ae478b6aa7fa
Michael Rulle <michael@rulle.xyz>
2023-11-08 11:29:20 +0100

    jeg smutter nu, vi snakkes i morgen

--- python/test.py
+++ python/test.py
@@ -8,7 +8,23 @@ session = cluster.connect()
 version = session.execute("SELECT release_version FROM system.local").one()
 print(f'connected to cassandra {version} :-)')
 
+create_stmt = """
+CREATE COLUMN FAMILY Posts
+  WITH comparator = UTF8Type
+  AND key_validation_class=UTF8Type
+  AND default_validation_class = UTF8Type
+  AND column_metadata = [
+    {column_name: author, validation_class: UTF8Type},
+    {column_name: title, validation_class: UTF8Type},
+    {column_name: content, validation_class: UTF8Type},
+    {column_name: date, validation_class: DateType}
+  ];
+  """
 
-yay = input("say nam nam")
+response = session.execute(create_stmt)
+print(response)
+
+
+yay = input('say nam nam')
 
 print(f'did you say {yay}')
